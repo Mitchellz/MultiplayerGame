06d91e1098131e53dc85c064613a75a08c52beff
Mitchellz <mitchellzoph@gmail.com>
2020-10-06 22:12:07 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1602040127,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/beanperson-sheet0.png",
+		"images/beanperson-sheet1.png",
+		"images/wall-sheet0.png",
+		"images/tiledbackground.png",
+		"images/coneofvision-sheet0.png",
+		"images/plainface-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js",
+		"penumbra.png",
+		"pathfind.js"
+	]
+}
